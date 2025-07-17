66bf9088d775926283f5ded805cde3e28f6c0beb
JavierVelas <velasco12bigchis@gmail.com>
2025-07-17 00:23:21 -0600

    co

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('../backend/src/router/auth.Routes');
+const authRoutes = require('./src/router/auth.Routes');
 
-const noticiasRoutes = require('../backend/src/router/noticias.Routes');
+const noticiasRoutes = require('./src/router/noticias.Routes');
 
 const app = express();
 
